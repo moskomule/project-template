21bc8702dbf879ccd6b82ebbf795dc966154f1a0
moskomule <hataya@keio.jp>
2020-07-14 20:48:43 +0900

    Add main.py as an example

---
+++ src/main.py
@@ -0,0 +1,29 @@
+import dataclasses
+
+import hydra
+import torch
+
+
+@dataclasses.dataclass
+class BaseConfig:
+    seed: int = 1
+    gpu_id: int = 0
+    verb: bool = False
+
+
+def _main(cfg: BaseConfig):
+    # actual main function
+    pass
+
+
+@hydra.main('config/config.yaml')
+def main(cfg: BaseConfig):
+    if cfg.verb:
+        print(cfg)
+    torch.cuda.set_device(cfg.gpu_id)
+    with homura.set_seed(cfg.seed):
+        _main(cfg)
+
+
+if __name__ == "__main__":
+    main()
